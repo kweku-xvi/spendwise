fa563f57388d26bde0296d132204eb1d682e2b87
kweku-xvi <nksarps@gmail.com>
2024-11-14 16:59:48 +0000

    added url endpoint for user registration

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kweku-xvi/spendwise/api/v1/controllers"
+	"github.com/kweku-xvi/spendwise/internal/database"
 )
+
+func init() {
+	database.ConnectDB()
+}
 
 func main() {
 	r := gin.Default()
@@ -12,6 +18,10 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
 	})
+	v1 := r.Group("/api/v1")
+	{
+		v1.POST("/users/signup", controllers.SignUp)
+	}
 
 	r.Run()
 }
