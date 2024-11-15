28cafd3772200cc77116012ee1834f613fee7ab5
kweku-xvi <nksarps@gmail.com>
2024-11-15 09:29:47 +0000

    added user log in controller

--- api/v1/controllers/auth.go
+++ api/v1/controllers/auth.go
@@ -2,10 +2,13 @@ package controllers
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/kweku-xvi/spendwise/api/v1/dto"
 	"github.com/kweku-xvi/spendwise/api/v1/models"
+	"github.com/kweku-xvi/spendwise/internal/config"
 	"github.com/kweku-xvi/spendwise/internal/database"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -47,5 +50,58 @@ func SignUp(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, gin.H{
 		"data":user,
+	})
+}
+
+func SignIn(c *gin.Context) {
+	var body dto.SignInRequest
+
+	if err := c.Bind(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":err,
+		})
+		return
+	}
+
+	var userFound models.User
+	database.DB.Where("email=?", body.Email).Find(&userFound)
+
+	if userFound.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":"user not found",
+		})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(userFound.Password), []byte(body.Password)); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":"invalid password",
+		})
+		return
+	}
+
+	generateToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id": userFound.ID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	token, err := generateToken.SignedString([]byte(config.ENV.JWTSecret))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":"failed to generate token",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"token":token,
+	})
+}
+
+func GetUserProfile(c *gin.Context) {
+	user, _ := c.Get("currentUser")
+
+	c.JSON(http.StatusOK, gin.H{
+		"user":user,
 	})
 }
