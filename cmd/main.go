df20797ac3c03923fcbdd47b35eb6944099ee8e9
kweku-xvi <nksarps@gmail.com>
2024-11-15 09:27:24 +0000

    add url endpoint for profile of current user

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/kweku-xvi/spendwise/api/v1/controllers"
+	"github.com/kweku-xvi/spendwise/api/v1/middleware"
 	"github.com/kweku-xvi/spendwise/internal/database"
 )
 
@@ -20,7 +21,9 @@ func main() {
 	})
 	v1 := r.Group("/api/v1")
 	{
-		v1.POST("/users/signup", controllers.SignUp)
+		v1.POST("/auth/signup", controllers.SignUp)
+		v1.POST("/auth/login", controllers.SignIn)
+		v1.GET("/user/profile", middleware.CheckAuth, controllers.GetUserProfile)
 	}
 
 	r.Run()
